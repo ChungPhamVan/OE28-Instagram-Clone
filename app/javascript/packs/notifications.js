c48b83900aec55a561f0224b8024739a1569f1dc
ChungPhamVan <chung.pv172983@sis.hust.edu.vn>
2020-03-24 18:33:21 +0700

    private-public-user

--- app/javascript/packs/notifications.js
+++ app/javascript/packs/notifications.js
@@ -1,36 +1,30 @@
 $(document).mouseup(function(e) {
-  var container = $('div.notif');
+  var container = $("div.notif");
   if (!container.is(e.target) && container.has(e.target).length === 0) {
-    $('div.notif').removeClass('d-block');
+    $("div.notif").removeClass("d-block");
   }
 });
-$(document).on('turbolinks:load', function() {
-  $('div#notification_icon')
-    .unbind('click')
-    .on('click', function() {
-      $('#count_unread_notif').hide();
-      $('div.notif').toggleClass('d-block');
+$(document).on("turbolinks:load", function() {
+  $("div#notification_icon")
+    .unbind("click")
+    .on("click", function() {
+      $("#count_unread_notif").hide();
+      $("div.notif").toggleClass("d-block");
     });
-  $('a.update_submit')
-    .unbind('click')
-    .on('click', function() {
-      $(this)
-        .parent()
-        .find('.button_update_notif')
-        .click();
-      $(this).remove();
-    });
-  $('a.delete_submit')
-    .unbind('click')
-    .on('click', function() {
-      $(this)
-        .parent()
-        .find('.button_delete_notif')
-        .click();
-    });
-  $('a#mark_all_as_read')
-    .unbind('click')
-    .on('click', function() {
-      $('.update_submit').click();
-    });
+  $(document).on("click", "a.update_submit", function() {
+    $(this)
+      .parent()
+      .find(".button_update_notif")
+      .click();
+    $(this).remove();
+  });
+  $(document).on("click", "a.delete_submit", function() {
+    $(this)
+      .parent()
+      .find(".button_delete_notif")
+      .click();
+  });
+  $(document).on("click", "a#mark_all_as_read", function() {
+    $(".update_submit").click();
+  });
 });
